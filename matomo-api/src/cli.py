94d79f191b666c04be1a1c845a3ed1e7fedc0d5c
MJedr <jedrych.marcysia@gmail.com>
2023-10-05 14:45:15 +0200

    update cli

--- matomo-api/src/cli.py
+++ matomo-api/src/cli.py
@@ -7,14 +7,16 @@ from api import MatomoAPI
 @click.command()
 @click.option("--date", "-d", default=date.today())
 def fetch_matomo_inspire_data(date: str):
+    click.echo(f"Fetching data for date {date}")
     if isinstance(date, str):
         try:
-            date_datetime = datetime.strptime(date, "%Y-%M-%d")
+            date = datetime.strptime(date, "%Y-%M-%d")
         except ValueError:
             click.echo("Wrong date format! Aborting.")
+            return
 
     matomo = MatomoAPI()
-    matomo.fetch_inspire_statistics(date_datetime)
+    matomo.fetch_inspire_statistics(date)
 
 
 if __name__ == "__main__":
